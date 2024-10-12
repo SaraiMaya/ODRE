7d41eee71fa932c65fbb5b9bf9dfbe30884d4b99
sarai maya <saraimaya190506@gmail.com>
2024-10-11 19:00:20 -0600

    primer comit

---
+++ archivo.js
@@ -0,0 +1,17 @@
+var nombre, apellido, edad;
+
+nombre = prompt("Ingrese su Nombre...");
+
+document.write ("Nombre: ", nombre);
+
+document.write ("<br>");
+
+apellido = prompt ("Ingrese su Apellido...");
+
+document.write ("Apellido: ", apellido);
+
+document.write ("<br>");
+
+edad = prompt ("Ingrese su Edad...");
+
+document.write ("Edad: ", edad);
